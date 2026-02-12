e8735aeb552497d3786ad1501b5ae6239ebd2ace
CAL1ST0 <isaaccalisto7@gmail.com>
2026-02-12 13:36:25 -0500

    update

--- webdev/jsDOM.js
+++ webdev/jsDOM.js
@@ -1,9 +1,11 @@
 function showFilter(){
-
+  let showFilter = document.getElementById("filterContent");
+  showFilter.style.display = "flex";
 }
 
 function showAddNew(){
-
+  let showArticle = document.getElementById("newContent");
+  showArticle.style.display = "flex";
 }
 
 function addNewArticle(){
