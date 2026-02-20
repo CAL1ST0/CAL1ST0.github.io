bf7c0dd10fa5b0948b5915b0f859ef0532c0c845
CAL1ST0 <isaaccalisto7@gmail.com>
2026-02-20 11:44:52 -0500

    update

--- webdev/pokeAPIJS.js
+++ webdev/pokeAPIJS.js
@@ -1,5 +1,5 @@
 const pokeAPI = "https://pokeapi.co/api/v2/pokemon/";
-
+let imgConst;
 
 function findPokemon(){
   let pokemon = document.getElementById("inputArea").value;
@@ -35,6 +35,8 @@ function parseObject(data){
   let image = data.sprites.front_default;
   let sound = data.cries.latest;
 
+  imgConst = image;
+  console.log(imgConst);
   //console.log(sound);
   //console.log(moves);
 
@@ -55,7 +57,22 @@ function parseObject(data){
     loadSound(sound);
   }
 
-  addMoves(moves);
+  if(document.getElementById("option") == null){
+    addMoves(moves, document.getElementById("select1"));
+    addMoves(moves, document.getElementById("select2"));
+    addMoves(moves, document.getElementById("select3"));
+    addMoves(moves, document.getElementById("select4"));
+  }
+
+  else{
+    removeMoves();
+    addMoves(moves, document.getElementById("select1"));
+    addMoves(moves, document.getElementById("select2"));
+    addMoves(moves, document.getElementById("select3"));
+    addMoves(moves, document.getElementById("select4"));
+  }
+
+  
 
 
 }
@@ -92,21 +109,80 @@ function loadSound(sound){
   audio.appendChild(source);
 }
 
-function addMoves(moves){
+function addMoves(moves, select){
+
+  for(let item of moves){ 
+    let option = document.createElement("option");
+    option.setAttribute("id", "option");
+    option.setAttribute("name", "option");
+    option.setAttribute("value", item.move.name);
+    option.innerHTML = item.move.name;
+    select.appendChild(option);
+  }
+}
+
+function removeMoves(){
   let select1 = document.getElementById("select1");
   let select2 = document.getElementById("select2");
   let select3 = document.getElementById("select3");
   let select4 = document.getElementById("select4");
 
-  for(let item of moves){ 
-    let option = document.createElement("option");
-    option.setAttribute("value", item);
-    option.innerHTML = item;
-    select1.appendChild(option);
-    select2.appendChild(option);
-    select3.appendChild(option);
-    select4.appendChild(option);
+  let option = document.getElementsByName("option");
+  console.log(option);
+  for(item of option){
+    option.removeChild(item);
   }
+
+}
+
+function addToTeam(){
+  let img = createTeamImage();
+  let list = createList();
+ 
+
+  let div = document.createElement("div");
+  div.setAttribute("width", "100%");
+
+  div.appendChild(img);
+  div.appendChild(list);
+
+  let team = document.getElementById("team");
+  team.appendChild(div);
+
+
+}
+
+function createList(){
+  let item1 = document.getElementById("select1").value;
+  let item2 = document.getElementById("select2").value;
+  let item3 = document.getElementById("select3").value;
+  let item4 = document.getElementById("select4").value;
+
+  let listItem1 = document.createElement("li");
+  let listItem2 = document.createElement("li");
+  let listItem3 = document.createElement("li");
+  let listItem4 = document.createElement("li");
+
+  listItem1.innerHTML = item1;
+  listItem2.innerHTML = item2;
+  listItem3.innerHTML = item3;
+  listItem4.innerHTML = item4;
+
+  let list = document.createElement("ul");
+  list.appendChild(listItem1);
+  list.appendChild(listItem2);
+  list.appendChild(listItem3);
+  list.appendChild(listItem4);
+
+  return list;
+}
+
+function createTeamImage(){
+  let img = document.createElement("img");
+  img.setAttribute("src", imgConst);
+  img.setAttribute("alt", "member");
+  img.setAttribute("id", "pokeIMGMember");
+  return img;
 }
 
 
